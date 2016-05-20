12521dad8a0a09589bdd6ce9c0951b73ffc2fb4b
TERRYQQY <qqy@qqydeMacBook-Pro.local>
2016-05-21 00:15:47 +0800

    learned def !

---
+++ Pra6.py
@@ -0,0 +1,16 @@
+#several functions
+abs(12.44)
+cmp(12, 44)
+int('12344')
+float('12.334')
+str(11.23)
+print bool(0)
+a = bool
+print a(1)
+a = input('enter a number')
+def a_abs(x):
+	if x >= 0:
+		return x
+	else:
+		return -x
+print a_abs(a)
